d6ac4d02c4427dd297384885def188c9128bb653
Thomas Nemeth <thonem10@gmail.com>
2021-04-08 10:46:46 +0200

    added rampage buff

---
+++ E2021Unity/Assets/Scripts/Rampage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rampage : MonoBehaviour
+{
+	// Start is called before the first frame update
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//Checks if the object colliding with powerup is the player
+		if (other.CompareTag("Player"))
+		{
+			PickUp(other);
+		}
+	}
+
+
+	public void PickUp(Collider other)
+	{
+		Debug.Log("Rampage");
+
+		Destroy(gameObject);
+	}
+}
