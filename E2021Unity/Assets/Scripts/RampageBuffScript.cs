8446e3a4e30a4b4150190b2e642a18ac2562fe10
Thomas Nemeth <thonem10@gmail.com>
2021-04-09 17:16:05 +0200

    added speed buff and rampage

---
+++ E2021Unity/Assets/Scripts/RampageBuffScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RampageBuffScript : MonoBehaviour
+{
+	// Start is called before the first frame update
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Colliding with" + other);
+		//Checks if the object colliding with powerup is the player
+		if (other.CompareTag("Player"))
+		{
+			Debug.Log("colliding with player");
+			PickUp(other);
+		}
+	}
+
+
+	public void PickUp(Collider other)
+	{
+		Debug.Log("Rampage");
+
+		Destroy(gameObject);
+	}
+}
---
+++ E2021Unity/Assets/Scripts/SpeedBuffScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class SpeedBuffScript : MonoBehaviour
+{
+	public float speedbuff = 1.5f;
+	public float time = 5f;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			//sending player collision to pickup function
+			StartCoroutine(PickUpSpeed(other));
+		}
+	}
+
+	private IEnumerator PickUpSpeed(Collider other)
+	{
+		Debug.Log("Speed engaged");
+		FirstPersonController stats = other.GetComponent<FirstPersonController>();
+		stats.m_RunSpeed *= speedbuff;
+
+
+		//wait some seconds before reverting the buff
+		yield return new WaitForSeconds(time);
+
+		//back to normal speed
+		stats.m_RunSpeed /= speedbuff;
+
+
+		Destroy(gameObject);
+	}
+}
+
+
