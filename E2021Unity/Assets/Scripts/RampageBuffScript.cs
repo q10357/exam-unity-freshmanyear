3027ff5dc2062a12578c0e9187403b51f6adc891
Thomas Nemeth <thonem10@gmail.com>
2021-04-09 17:36:20 +0200

    rampage working

--- E2021Unity/Assets/Scripts/RampageBuffScript.cs
+++ E2021Unity/Assets/Scripts/RampageBuffScript.cs
@@ -6,22 +6,35 @@ public class RampageBuffScript : MonoBehaviour
 {
 	// Start is called before the first frame update
 
-
+	public float damagebuff = 3f;
+	public float durationbuff = 5f;
 	private void OnTriggerEnter(Collider other)
 	{
 		Debug.Log("Colliding with" + other);
 		//Checks if the object colliding with powerup is the player
 		if (other.CompareTag("Player"))
 		{
-			Debug.Log("colliding with player");
-			PickUp(other);
+			StartCoroutine(PickUpRampage(other));
 		}
 	}
 
 
-	public void PickUp(Collider other)
+	public IEnumerator PickUpRampage(Collider other)
 	{
 		Debug.Log("Rampage");
+
+		Gunshooting gunstats = other.GetComponentInChildren<Gunshooting>();
+		
+		//adding the damage buff
+		gunstats.damage *= damagebuff;
+		
+		//wait for the duration
+
+		yield return new WaitForSeconds(durationbuff);
+
+		//reverting to noraml stats
+
+		gunstats.damage /= damagebuff;
 
 		Destroy(gameObject);
 	}
